c93dbcefe8bcef5eeb167b23610edf6b21006023
panwenkai <wenkai@myhostname.localdomain>
2017-02-28 23:09:44 -0500

    Fix part 4

--- test/hw4-test.c
+++ test/hw4-test.c
@@ -13,14 +13,15 @@ int main(int argc, char *argv[])
 	int return_value = syscall(400, parent_pid_test, &parent_euid);
 	if (return_value < 0)
 	{
-		printf("Return value is: %d", return_value);
+		printf("Return value from test 1 is: %d\n", return_value);
 		perror("Given pid is not the pid of the calling process's parent process.");
 	}
 
 	// Test2: Given pid is the pid of the calling process's parent process.
 	parent_pid_test = parent_pid_true;
 	return_value = syscall(400, parent_pid_test, &parent_euid);
-	printf("Return value is: %d", return_value);
+	printf("Return value from test 2 is: %d\n", return_value);
+	printf("Parent euid is: %d\n", parent_euid);
 	if (return_value < 0)
 	{
 		perror("The effective user of the parent is NOT root.");
